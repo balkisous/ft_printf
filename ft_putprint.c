e52583339b5044ac558fb3189e0b10928362ee3a
Balkis BEN YAAGOUB <bben-yaa@e2r11p6.42.fr>
2021-06-25 14:32:23 +0200

    changement des noms de ficherrs changment de str_type test pour s a 64

---
+++ ft_putprint.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putprint.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bben-yaa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/02 15:20:37 by bben-yaa          #+#    #+#             */
+/*   Updated: 2021/06/25 14:29:16 by bben-yaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_stock_type(struct s_env *p, va_list	nb_arg)
+{
+	if (p->f.type == 'c')
+		p->t.c = va_arg(nb_arg, int);
+	else if (p->f.type == 's')
+		p->t.s = va_arg(nb_arg, char *);
+	if(!p->t.s)
+		p->t.s = "(null)";
+	//if (p->f.type == 's' && p->t.s == 0)  --> ne fonctionne pas
+		//p->t.s = "0";
+	else if (p->f.type == 'd' || p->f.type == 'i')
+		p->t.n = va_arg(nb_arg, int);
+	else if (p->f.type == 'p')
+		p->t.p = va_arg(nb_arg, long long unsigned int *);
+	else if (p->f.type == 'u' || p->f.type == 'x' || p->f.type == 'X')
+		p->t.u = va_arg(nb_arg, unsigned int);
+	p->f.size_arg = ft_size_arg(p);
+	return(ft_print_flags(p));
+}
+
+int		ft_putchar(int c)		/////ft_putchar////
+{
+	char b;
+
+	b = (char)c;
+	write (1, &b, 1);
+	return (1);
+}
+int 	ft_putstr(char *str, int leng)	/////ft_putstr/////
+{
+	int i;
+
+	i = 0;
+	while (str[i] && leng)
+	{
+		write(1, &str[i], 1);
+		i++;
+		leng--;
+	}
+	return (i);
+}
+
+int			ft_putnbr(long int n)	////ft_putnbr////
+{
+	int		ret;
+	int		i;
+
+	i = n;
+	ret = ft_nb_leng(i);
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + '0');
+	else if (n < 0)
+	{
+			ft_putchar('-');
+			ft_putnbr(-n);
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	return (ret);
+}
+
+int		ft_putnbr_unsigned(unsigned int u)
+{							
+	int				ret;						////pas tester 
+	unsigned int 	i;
+	
+	i = u;
+	ret = ft_nb_leng(i);
+	if (u >= 0 && u <= 9)
+		ft_putchar(u + '0');
+	else
+	{
+		ft_putnbr_unsigned(u / 10);
+		ft_putnbr_unsigned(u % 10);
+	}
+	return (ret);
+}
+int		ft_putptr(long long unsigned int u)		////////ft_putptr//////
+{
+	char	*s;
+	char	c;
+	int		ret;
+
+	ret = 0;
+	s = "0123456789abcdef";
+//	ret += ft_putstr("0x");      //// il manque 0x au debut + il manque +3 a ret 
+	if(u / 16)
+		ret += ft_putptr(u / 16);
+	c = s[(u % 16)];
+	ret += write(1, &c, 1);
+	return (ret);
+}
+
+int		ft_putnbr_hexa(unsigned int u)		//////ft_putnbr_hexa////////
+{
+	char	*s;
+
+	s = "0123456789abcdef";
+	if (u / 16)
+	ft_putnbr_hexa(u/ 16);
+	ft_putchar(s[(u % 16)]);
+	return (ft_nb_hexa_leng(u));
+}
+
+int		ft_putnbr_heXa(unsigned int n)		////////ft_putnbr_heXa////////
+{
+	char	*s;
+
+	s = "0123456789ABCDEF";
+	if (n / 16)
+	ft_putnbr_heXa(n / 16);
+	ft_putchar(s[(n % 16)]);
+	return (ft_nb_hexa_leng(n));
+}
---
+++ ft_speciaux_type.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_speciaux_type.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bben-yaa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/21 11:41:06 by bben-yaa          #+#    #+#             */
+/*   Updated: 2021/06/25 14:29:18 by bben-yaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void		ft_speciaux_type(struct	s_env	*p)
+{
+	if (p->t.s)
+	{
+		if (p->f.intprecision || p->f.largeur )
+			ft_str_type(p);
+	}
+	else if (p->t.c)
+		ft_char_type(p);
+	else if (p->t.p)
+	{
+		if (p->f.intprecision || (p->f.i == '0' && p->f.largeur &&
+				   	p->f.precision != '.'))
+			ft_ptr_type(p);
+	}
+}
+
+void		ft_str_type(struct	s_env		*p)
+{
+
+	if (p->f.intprecision && !p->f.largeur)
+	{
+		if(p->f.intprecision >= p->f.size_arg)
+			p->f.size_arg = p->f.size_arg;
+		else if (p->f.intprecision < p->f.size_arg)
+			p->f.size_arg = p->f.intprecision;
+		p->f.intprecision = 0;
+		p->t.y = 0;
+	}
+	else if (p->f.intprecision && p->f.largeur)
+	{
+		if (p->f.intprecision > p->f.size_arg && p->f.intprecision > p->f.largeur && 
+				p->f.largeur <= p->f.size_arg)
+		{
+			p->f.largeur = 0;
+			p->t.y = 0;
+		}
+		else if (p->f.intprecision > p->f.largeur && p->f.intprecision < p->f.size_arg &&
+			p->f.largeur <= p->f.size_arg)
+		{
+			p->f.largeur = 0;
+			p->f.size_arg = p->f.intprecision;
+			p->t.y = 0;
+		}
+		else if (p->f.largeur > p->f.intprecision)
+		{
+			p->f.largeur -= p->f.intprecision;
+			p->t.y = 0;
+		}
+	}
+}
+
+void	ft_char_type(struct s_env		*p)
+{
+	if (p->f.largeur && p->f.i == '0' && p->f.precision == '.')
+	{	
+		p->f.intprecision = p->f.largeur;
+		p->f.largeur = 0;
+	}
+	else if ((p->f.intprecision && p->f.largeur == 0) ||
+			(p->f.intprecision && p->f.largeur))
+		p->f.intprecision = 0;
+}
+
+void	ft_ptr_type(struct s_env		*p)
+{
+	if (p->f.intprecision)
+	{
+		p->t.y = 0;
+		p->f.intprecision += 2;
+	}
+	else if ((p->f.i == '0' && p->f.largeur) || (p->f.i == '0' &&
+			   	p->f.largeur && p->f.precision != '.'))
+		p->t.y = 0;
+}
+
+void	ft_print_0x(struct s_env		*p)
+{
+	if (p->t.p)
+	{
+		if (p->t.y == 0)
+			p->f.ret += ft_putstr("0x", 2);
+	}
+}
